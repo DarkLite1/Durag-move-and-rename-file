0417be89ce86c6f5cecfa5b65909b16fc533f6ff
DarkLite1 <BrechtGijbels@gmail.com>
2025-04-02 14:18:52 +0200

    script born

---
+++ Durag move and rename file.Tests.ps1
@@ -0,0 +1,255 @@
+﻿#Requires -Modules Pester
+#Requires -Version 7
+
+BeforeAll {
+    $realCmdLet = @{
+        OutFile = Get-Command Out-File
+    }
+
+    $testInputFile = @{
+        Source                            = @{
+            Folder             = (New-Item 'TestDrive:/s' -ItemType Directory).FullName
+            MatchFileNameRegex = 'Analyse_[0-9]{8}.xlsx'
+        }
+        Destination                       = @{
+            Folder = (New-Item 'TestDrive:/d' -ItemType Directory).FullName
+        }
+        ProcessFilesInThePastNumberOfDays = 1
+    }
+
+    $testOutParams = @{
+        FilePath = (New-Item "TestDrive:/Test.json" -ItemType File).FullName
+    }
+
+    $testScript = $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+    $testParams = @{
+        ScriptName = 'Test (Brecht)'
+        ImportFile = $testOutParams.FilePath
+        LogFolder  = New-Item 'TestDrive:/log' -ItemType Directory
+    }
+
+    Mock Out-File
+}
+Describe 'the mandatory parameters are' {
+    It '<_>' -ForEach @('ImportFile') {
+        (Get-Command $testScript).Parameters[$_].Attributes.Mandatory |
+        Should -BeTrue
+    }
+}
+Describe 'create an error log file when' {
+    It 'the log folder cannot be created' {
+        $testNewParams = $testParams.clone()
+        $testNewParams.LogFolder = 'xxx:://notExistingLocation'
+
+        .$testScript @testNewParams
+
+        Should -Invoke Out-File -Times 1 -Exactly -ParameterFilter {
+            ($FilePath -like '*\Error.txt') -and
+            ($InputObject -like '*Failed creating the log folder*')
+        }
+    }
+    Context 'the ImportFile' {
+        It 'is not found' {
+            $testNewParams = $testParams.clone()
+            $testNewParams.ImportFile = 'nonExisting.json'
+
+            .$testScript @testNewParams
+
+            Should -Invoke Out-File -Times 1 -Exactly -ParameterFilter {
+                ($FilePath -like '* - Error.txt') -and
+                ($InputObject -like '*Cannot find path*nonExisting.json*')
+            }
+        }
+        Context 'property' {
+            It '<_> not found' -ForEach @(
+                'Folder', 'MatchFileNameRegex'
+            ) {
+                $testNewInputFile = Copy-ObjectHC $testInputFile
+                $testNewInputFile.Source.$_ = $null
+
+                & $realCmdLet.OutFile @testOutParams -InputObject (
+                    $testNewInputFile | ConvertTo-Json -Depth 7
+                )
+
+                .$testScript @testParams
+
+                Should -Invoke Out-File -Times 1 -Exactly -ParameterFilter {
+                    ($FilePath -like '* - Error.txt') -and
+                    ($InputObject -like "*$ImportFile*Property 'Source.$_' not found*")
+                }
+            }
+            It '<_> not found' -ForEach @(
+                'Folder'
+            ) {
+                $testNewInputFile = Copy-ObjectHC $testInputFile
+                $testNewInputFile.Destination.$_ = $null
+
+                & $realCmdLet.OutFile @testOutParams -InputObject (
+                    $testNewInputFile | ConvertTo-Json -Depth 7
+                )
+
+                .$testScript @testParams
+
+                Should -Invoke Out-File -Times 1 -Exactly -ParameterFilter {
+                    ($FilePath -like '* - Error.txt') -and
+                    ($InputObject -like "*$ImportFile*Property 'Destination.$_' not found*")
+                }
+            }
+            It 'Folder <_> not found' -ForEach @(
+                'Source', 'Destination'
+            ) {
+                $testNewInputFile = Copy-ObjectHC $testInputFile
+                $testNewInputFile[$_]['Folder'] = 'TestDrive:\nonExisting'
+
+                & $realCmdLet.OutFile @testOutParams -InputObject (
+                    $testNewInputFile | ConvertTo-Json -Depth 7
+                )
+
+                .$testScript @testParams
+
+                Should -Invoke Out-File -Times 1 -Exactly -ParameterFilter {
+                    ($FilePath -like '* - Error.txt') -and
+                    ($InputObject -like "*$ImportFile*$_.Folder 'TestDrive:\nonExisting' not found*")
+                }
+            }
+            Context 'ProcessFilesInThePastNumberOfDays' {
+                It 'is not a number' {
+                    $testNewInputFile = Copy-ObjectHC $testInputFile
+                    $testNewInputFile.ProcessFilesInThePastNumberOfDays = 'a'
+
+                    & $realCmdLet.OutFile @testOutParams -InputObject (
+                        $testNewInputFile | ConvertTo-Json -Depth 7
+                    )
+
+                    .$testScript @testParams
+
+                    Should -Invoke Out-File -Times 1 -Exactly -ParameterFilter {
+                        ($FilePath -like '* - Error.txt') -and
+                        ($InputObject -like "*$ImportFile*Property 'ProcessFilesInThePastNumberOfDays' must be 0 or a positive number. Number 0 processes all files in the source folder. The value 'a' is not supported*")
+                    }
+                }
+                It 'is a negative number' {
+                    $testNewInputFile = Copy-ObjectHC $testInputFile
+                    $testNewInputFile.ProcessFilesInThePastNumberOfDays = -1
+
+                    & $realCmdLet.OutFile @testOutParams -InputObject (
+                        $testNewInputFile | ConvertTo-Json -Depth 7
+                    )
+
+                    .$testScript @testParams
+
+                    Should -Invoke Out-File -Times 1 -Exactly -ParameterFilter {
+                        ($FilePath -like '* - Error.txt') -and
+                        ($InputObject -like "*$ImportFile*Property 'ProcessFilesInThePastNumberOfDays' must be 0 or a positive number. Number 0 processes all files in the source folder. The value '-1' is not supported*")
+                    }
+                }
+                It 'is an empty string' {
+                    $testNewInputFile = Copy-ObjectHC $testInputFile
+                    $testNewInputFile.ProcessFilesInThePastNumberOfDays = ''
+
+                    & $realCmdLet.OutFile @testOutParams -InputObject (
+                        $testNewInputFile | ConvertTo-Json -Depth 7
+                    )
+
+                    .$testScript @testParams
+
+                    Should -Invoke Out-File -Times 1 -Exactly -ParameterFilter {
+                        ($FilePath -like '* - Error.txt') -and
+                        ($InputObject -like "*$ImportFile*Property 'ProcessFilesInThePastNumberOfDays' must be 0 or a positive number. Number 0 processes all files in the source folder. The value '' is not supported*")
+                    }
+                }
+                It 'is missing' {
+                    $testNewInputFile = @{
+                        Source      = $testInputFile.Source
+                        Destination = $testInputFile.Destination
+                    }
+
+                    & $realCmdLet.OutFile @testOutParams -InputObject (
+                        $testNewInputFile | ConvertTo-Json -Depth 7
+                    )
+
+                    .$testScript @testParams
+
+                    Should -Invoke Out-File -Times 1 -Exactly -ParameterFilter {
+                        ($FilePath -like '* - Error.txt') -and
+                        ($InputObject -like "*$ImportFile*Property 'ProcessFilesInThePastNumberOfDays' must be 0 or a positive number. Number 0 processes all files in the source folder. The value '' is not supported*")
+                    }
+                }
+                It '0 is accepted' {
+                    $testNewInputFile = Copy-ObjectHC $testInputFile
+                    $testNewInputFile.ProcessFilesInThePastNumberOfDays = '0'
+
+                    & $realCmdLet.OutFile @testOutParams -InputObject (
+                        $testNewInputFile | ConvertTo-Json -Depth 7
+                    )
+
+                    .$testScript @testParams
+
+                    Should -Invoke -Not Out-File -ParameterFilter {
+                        ($FilePath -like '* - Error.txt') -and
+                        ($InputObject -like "*ProcessFilesInThePastNumberOfDays*")
+                    }
+                }
+            }
+        }
+    }
+}
+Describe 'when the source folder is empty' {
+    It 'no error log file is created' {
+        $testNewInputFile = Copy-ObjectHC $testInputFile
+        $testNewInputFile.Source.Folder = (New-Item 'TestDrive:/empty' -ItemType Directory).FullName
+
+        & $realCmdLet.OutFile @testOutParams -InputObject (
+            $testNewInputFile | ConvertTo-Json -Depth 7
+        )
+
+        .$testScript @testParams
+
+        Should -Not -Invoke Out-File
+    }
+}
+Describe 'when there is a file in the source folder' {
+    It 'the file is copied to the destination folder with the correct name' {
+        $testNewInputFile = Copy-ObjectHC $testInputFile
+
+        $testNewInputFile.Source.Folder = (New-Item 'TestDrive:/source' -ItemType Directory).FullName
+        $testNewInputFile.Destination.Folder = (New-Item 'TestDrive:/destination' -ItemType Directory).FullName
+
+        New-Item "$($testNewInputFile.Source.Folder)\Analyse_26032025.xlsx" -ItemType File
+
+        & $realCmdLet.OutFile @testOutParams -InputObject (
+            $testNewInputFile | ConvertTo-Json -Depth 7
+        )
+
+        .$testScript @testParams
+
+        Get-Item "$($testNewInputFile.Destination.Folder)\2025\AnalysesJour_20250326.xlsx" |
+        Should -Not -BeNullOrEmpty
+    }
+}
+Describe 'when a file fails to copy' {
+    BeforeAll {
+        Mock Copy-Item {
+            throw 'Oops'
+        }
+
+        $testNewInputFile = Copy-ObjectHC $testInputFile
+
+        $testNewInputFile.Source.Folder = (New-Item 'TestDrive:/source' -ItemType Directory).FullName
+        $testNewInputFile.Destination.Folder = (New-Item 'TestDrive:/destination' -ItemType Directory).FullName
+
+        $testFile = New-Item "$($testNewInputFile.Source.Folder)\Analyse_26032025.xlsx" -ItemType File
+
+        & $realCmdLet.OutFile @testOutParams -InputObject (
+            $testNewInputFile | ConvertTo-Json -Depth 7
+        )
+
+        .$testScript @testParams
+    }
+    It 'an error log file is created' {
+        Should -Invoke Out-File -Times 1 -Exactly -Scope Describe -ParameterFilter {
+            ($FilePath -like '* - Error.txt') -and
+            ($InputObject -like "*Failure for source file*Failed to copy file '$($testFile.FullName)'*")
+        }
+    }
+}
---
+++ Durag move and rename file.ps1
@@ -0,0 +1,243 @@
+﻿#Requires -Version 7
+
+<#
+    .SYNOPSIS
+        Move files from the source folder to the destination folder with a new
+        name.
+
+    .DESCRIPTION
+        This script selects all files in the source folder that match the
+        'MatchFileNameRegex'.
+
+        The selected files are copied from the source folder to the destination
+        folder with a new name. The new name is based on the date string
+        available withing the source file name.
+
+    .PARAMETER ImportFile
+        A .JSON file that contains all the parameters used by the script.
+
+    .PARAMETER SourceFolder
+        The source folder.
+
+    .PARAMETER MatchFileNameRegex
+        Only files that match the regex will be copied.
+
+    .PARAMETER DestinationFolder
+        The destination folder.
+
+    .PARAMETER ProcessFilesInThePastNumberOfDays
+        Number of days in the past for which to process files.
+
+        Example:
+        - 0 : Process all files in the source folder, no filter
+        - 1 : Process files created since yesterday morning
+        - 5 : Process files created in the last 5 days
+
+    .PARAMETER LogFolder
+        The folder where the error log files will be saved.
+#>
+
+[CmdLetBinding()]
+param (
+    [Parameter(Mandatory)]
+    [string]$ImportFile,
+    [string]$ScriptName = 'Process computer actions',
+    [string]$LogFolder = "$PSScriptRoot\..\Log"
+)
+
+begin {
+    $ErrorActionPreference = 'stop'
+
+    try {
+        $ScriptStartTime = Get-Date
+
+        #region Create log folder
+        try {
+            $logParams = @{
+                LogFolder    = New-Item -Path $LogFolder -ItemType 'Directory' -Force -EA Stop
+                Name         = $ScriptName
+                Date         = 'ScriptStartTime'
+                NoFormatting = $true
+            }
+            $logFile = '{0} - Error.txt' -f (New-LogFileNameHC @LogParams)
+        }
+        catch {
+            Write-Warning $_
+
+            $params = @{
+                FilePath = "$PSScriptRoot\..\Error.txt"
+            }
+            "Failure:`r`n`r`n- Failed creating the log folder '$LogFolder': $_" | Out-File @params
+
+            exit
+        }
+        #endregion
+
+        try {
+            #region Import .json file
+            Write-Verbose "Import .json file '$ImportFile'"
+
+            $jsonFileContent = Get-Content $ImportFile -Raw -Encoding UTF8 |
+            ConvertFrom-Json
+            #endregion
+
+            $SourceFolder = $jsonFileContent.Source.Folder
+            $MatchFileNameRegex = $jsonFileContent.Source.MatchFileNameRegex
+            $DestinationFolder = $jsonFileContent.Destination.Folder
+
+            #region Test .json file properties
+            @(
+                'Folder', 'MatchFileNameRegex'
+            ).where(
+                { -not $jsonFileContent.Source.$_ }
+            ).foreach(
+                { throw "Property 'Source.$_' not found" }
+            )
+
+            @(
+                'Folder'
+            ).where(
+                { -not $jsonFileContent.Destination.$_ }
+            ).foreach(
+                { throw "Property 'Destination.$_' not found" }
+            )
+
+            #region Test integer value
+            try {
+                if ($jsonFileContent.ProcessFilesInThePastNumberOfDays -eq '') {
+                    throw 'a blank string is not supported'
+                }
+
+                [int]$ProcessFilesInThePastNumberOfDays = $jsonFileContent.ProcessFilesInThePastNumberOfDays
+
+                if ($jsonFileContent.ProcessFilesInThePastNumberOfDays -lt 0) {
+                    throw 'a negative number is not supported'
+                }
+            }
+            catch {
+                throw "Property 'ProcessFilesInThePastNumberOfDays' must be 0 or a positive number. Number 0 processes all files in the source folder. The value '$($jsonFileContent.ProcessFilesInThePastNumberOfDays)' is not supported."
+            }
+            #endregion
+            #endregion
+
+            #region Test folders exist
+            @{
+                'Source.Folder'      = $SourceFolder
+                'Destination.Folder' = $DestinationFolder
+            }.GetEnumerator().ForEach(
+                {
+                    $key = $_.Key
+                    $value = $_.Value
+
+                    if (!(Test-Path -LiteralPath $value -PathType Container)) {
+                        throw "$key '$value' not found"
+                    }
+                }
+            )
+            #endregion
+        }
+        catch {
+            throw "Input file '$ImportFile': $_"
+        }
+    }
+    catch {
+        Write-Warning $_
+        "Failure:`r`n`r`n- $_" | Out-File -FilePath $logFile -Append
+        exit
+    }
+}
+
+process {
+    try {
+        #region Get files from source folder
+        Write-Verbose "Get files in source folder '$SourceFolder'"
+
+        $params = @{
+            LiteralPath = $SourceFolder
+            Recurse     = $true
+            File        = $true
+        }
+        $filesToProcess = @(Get-ChildItem @params | Where-Object {
+                $_.Name -match $MatchFileNameRegex
+            }
+        )
+
+        if (!$filesToProcess) {
+            Write-Verbose 'No files found, exit script'
+            exit
+        }
+        #endregion
+
+        foreach ($file in $filesToProcess) {
+            try {
+                Write-Verbose "Processing file '$($file.FullName)'"
+
+                #region Create new file name
+                $year = $file.Name.Substring(12, 4)
+                $month = $file.Name.Substring(10, 2)
+                $day = $file.Name.Substring(8, 2)
+
+                $newFileName = "AnalysesJour_$($year)$($month)$($day).xlsx"
+
+                Write-Verbose "New file name '$newFileName'"
+                #endregion
+
+                #region Create destination folder
+                try {
+                    $params = @{
+                        Path      = $DestinationFolder
+                        ChildPath = $year
+                    }
+                    $destinationFolder = Join-Path @params
+
+                    Write-Verbose "Destination folder '$destinationFolder'"
+
+                    $params = @{
+                        LiteralPath = $destinationFolder
+                        PathType    = 'Container'
+                    }
+                    if (-not (Test-Path @params)) {
+                        $params = @{
+                            Path     = $destinationFolder
+                            ItemType = 'Directory'
+                            Force    = $true
+                        }
+
+                        Write-Verbose 'Create destination folder'
+
+                        $null = New-Item @params
+                    }
+                }
+                catch {
+                    throw "Failed to create destination folder '$destinationFolder': $_"
+                }
+                #endregion
+
+                #region Copy file to destination folder
+                try {
+                    $params = @{
+                        LiteralPath = $file.FullName
+                        Destination = "$($destinationFolder)\$newFileName"
+                        Force       = $true
+                    }
+
+                    Write-Verbose "Copy file '$($params.LiteralPath)' to '$($params.Destination)'"
+
+                    Copy-Item @params
+                }
+                catch {
+                    throw "Failed to copy file '$($params.LiteralPath)' to '$($params.Destination)': $_"
+                }
+                #endregion
+            }
+            catch {
+                Write-Warning $_
+                "Failure for source file '$($file.FullName)':`r`n`r`n- $_" | Out-File -FilePath $logFile -Append
+            }
+        }
+    }
+    catch {
+        Write-Warning $_
+        "Failure:`r`n`r`n- $_" | Out-File -FilePath $logFile -Append
+    }
+}

6f739416b07eb0bbf718f3c82a56cf415bf9f8f1
DarkLite1 <BrechtGijbels@gmail.com>
2025-04-07 16:40:12 +0200

    add event log

--- Durag move and rename file.ps1
+++ Durag move and rename file.ps1
@@ -59,9 +59,19 @@ begin {
 
     $systemErrors = @()
     $logFileData = [System.Collections.Generic.List[PSObject]]::new()
+    $eventLogData = [System.Collections.Generic.List[PSObject]]::new()
     $scriptStartTime = Get-Date
 
     try {
+        $eventLogData.Add(
+            [PSCustomObject]@{
+                DateTime  = $scriptStartTime
+                Message   = 'Script started'
+                EntryType = 'Information'
+                EventID   = '100'
+            }
+        )
+
         #region Import .json file
         Write-Verbose "Import .json file '$ImportFile'"
 
@@ -359,9 +369,95 @@ end {
         }
     }
 
+    function Write-EventsToEventLogHC {
+        <#
+        .SYNOPSIS
+            Import all the parameters for writing to the event log.
+
+        .DESCRIPTION
+            Import all the necessary parameters for writing to the event log. The
+            use of this function will allow standardization in the Windows Event Log
+            by using the same EventID's and other properties across different
+            scripts.
+
+            Custom Windows EventID's based on the PowerShell standard streams:
+
+            PowerShell Stream     EventIcon    EventID   EventDescription
+            -----------------     ---------    -------   ----------------
+                                  [i] Info     100       Script started
+            [4] Verbose           [i] Info     4         Verbose message
+            [1] Output/Success    [i] Info     1         Output on success
+                                  [i] Info     199       Script ended successfully
+            [3] Warning           [w] Warning  3         Warning message
+            [2] Error             [e] Error    2         Fatal error message
+
+        .PARAMETER Source
+            Specifies the script name under which the events will be logged.
+
+        .EXAMPLE
+            $ScriptName = 'Test'
+            Write-EventsToEventLogHC -Source $ScriptName
+            Write-EventLog @EventStartParams
+            Write-EventLog @EventVerboseParams -Message 'This is a verbose message'
+            Write-EventLog @EventOutParams -Message 'Send e-mail to the user'
+            Write-EventLog @EventEndParams
+
+            Writes the start and end time to the Windows Event Log together with
+            information from the verbose and output stream.
+        #>
+
+        [CmdLetBinding()]
+        [OutputType()]
+        param (
+            [Parameter(Mandatory)]
+            [String]$Source,
+            [Parameter(Mandatory)]
+            [String]$LogName
+        )
+
+        if (
+            -not(
+                ([System.Diagnostics.EventLog]::Exists($LogName)) -and
+                [System.Diagnostics.EventLog]::SourceExists($Source)
+            )
+        ) {
+            New-EventLog -LogName $LogName -Source $Source -ErrorAction Stop
+        }
+
+        $EventParams = @{
+            LogName     = $LogName
+            Source      = $Source
+            ErrorAction = 'Stop'
+        }
+        $Global:EventStartParams = $EventParams + @{
+            EntryType = 'Information'
+            EventID   = '100'
+            Message   = ($env:USERNAME + ' - ' + 'Script started')
+        }
+        $Global:EventEndParams = $EventParams + @{
+            EntryType = 'Information'
+            EventID   = '199'
+            Message   = ($env:USERNAME + ' - ' + 'Script ended')
+        }
+        $Global:EventVerboseParams = $EventParams + @{
+            EntryType = 'Information'
+            EventID   = '4'
+        }
+        $Global:EventOutParams = $EventParams + @{
+            EntryType = 'Information'
+            EventID   = '1'
+        }
+        $Global:EventWarnParams = $EventParams + @{
+            EntryType = 'Warning'
+            EventID   = '3'
+        }
+        $Global:EventErrorParams = $EventParams + @{
+            EntryType = 'Error'
+            EventID   = '2'
+        }
+    }
+
     try {
-
-
         $scriptName = $jsonFileContent.Settings.ScriptName
         $logFolder = $jsonFileContent.Settings.Log.Where.Folder
         $logFileExtensions = $jsonFileContent.Settings.Log.Where.FileExtensions
@@ -470,14 +566,34 @@ end {
 
         #region Write events to event log
         if ($logToEventLog) {
-            $systemErrors | ForEach-Object {
-                $eventLogParams = @{
-                    Source    = $scriptName
-                    Message   = $_.Error
-                    EntryType = 'Error'
+            $eventLogData.Add(
+                [PSCustomObject]@{
+                    DateTime  = Get-Date
+                    Message   = 'Script ended'
+                    EntryType = 'Information'
+                    EventID   = '199'
                 }
-                Write-EventLog @eventLogParams
+            )
+
+            $systemErrors | ForEach-Object {
+                $eventLogData.Add(
+                    [PSCustomObject]@{
+                        DateTime  = Get-Date
+                        Message   = $_.Error
+                        EntryType = 'Error'
+                        EventID   = '2'
+                    }
+                )
             }
+
+            $params = @{
+                Source  = $scriptName
+                LogName = 'HCScripts'
+                Events  = $eventLogData
+            }
+            Write-EventsToEventLogHC @params
+
+
         }
         else {
             Write-Verbose "Input file option 'Settings.Log.Where.EventLog' not true, no event log created."
